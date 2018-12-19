14e3c9df7c6d50d8c2fe6d61a434777b74759079
perdugames <perdugames@gmail.com>
2018-12-19 19:57:55 -0200

    add getListenerCountOfEvent and once function.

--- tests/unit/eventbus_test.js
+++ tests/unit/eventbus_test.js
@@ -2,7 +2,7 @@ require('../cacau-1.1.0.js');
 
 import EventBus from 'LIB/eventbus.js';
 
-TEST('eventbus_test.js',
+TEST('EventBus',
      
     TEST_F('TestGetListenerCountOfEventCheckEventListenersLength', () => {
         const eventBus = new EventBus();
@@ -10,9 +10,21 @@ TEST('eventbus_test.js',
         const expectedQuantityListeners = 1;
     
         const listener1 = function() {};
-        eventBus.events['event1'] = []listener1;
+        eventBus.events['event1'] = [listener1];
     
-        const actualQuantityListeners = eventBus.getListenerCountOfEvent();
+        const actualQuantityListeners = eventBus.getListenerCountOfEvent('event1');
+        
+        CHECK_ACTUAL_EQUAL_EXPECTED(actualQuantityListeners, expectedQuantityListeners);
+    }),
+     
+    TEST_F('TestOnceCheckEvents', () => {
+        const eventBus = new EventBus();
+    
+        const expectedQuantityListeners = 1;
+    
+        eventBus.once('event1', function() {});
+    
+        const actualQuantityListeners = eventBus.getListenerCountOfEvent('event1');
         
         CHECK_ACTUAL_EQUAL_EXPECTED(actualQuantityListeners, expectedQuantityListeners);
     }),
