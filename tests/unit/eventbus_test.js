7c21c6170145267b390d25a658b1f83582460e30
perdugames <perdugames@gmail.com>
2018-12-20 19:18:48 -0200

    new emit function.

--- tests/unit/eventbus_test.js
+++ tests/unit/eventbus_test.js
@@ -59,6 +59,67 @@ TEST('EventBus',
         CHECK_ACTUAL_EQUAL_EXPECTED(actualQuantityListeners, expectedQuantityListeners);
     }),
      
-   
+    TEST_F('TestOnAndEmitWithoutArgsCheckChangeMockObjInListenerFunction', () => {
+        const eventBus = new EventBus();
+        const mockX = {x: 0};
+    
+        const expectedX = mockX.x + 1;
+    
+        eventBus.on('event1', function() { mockX.x++; });
+        eventBus.emit('event1');
+    
+        const actualX = mockX.x;
+
+        CHECK_ACTUAL_EQUAL_EXPECTED(actualX, expectedX);
+    }),
+     
+    TEST_F('TestOnceAndEmitWithoutArgsCheckChangeMockObjInListenerFunction', () => {
+        const eventBus = new EventBus();
+        const mockX = {x: 0};
+    
+        const expectedX = mockX.x + 1;
+    
+        eventBus.once('event1', function() { mockX.x++; });
+        eventBus.emit('event1');
+        eventBus.emit('event1');
+    
+        const actualX = mockX.x;
+
+        CHECK_ACTUAL_EQUAL_EXPECTED(actualX, expectedX);
+    }),
+     
+    TEST_F('TestOnAndEmitWithOneArgCheckChangeMockObjInListenerFunction', () => {
+        const eventBus = new EventBus();
+        const mockX = {x: 0};
+    
+        const expectedX = mockX.x + 1;
+    
+        eventBus.on('event1', function(obj) { obj.x++; });
+        eventBus.emit('event1', mockX);
+    
+        const actualX = mockX.x;
+
+        CHECK_ACTUAL_EQUAL_EXPECTED(actualX, expectedX);
+    }),
+
+    TEST_F('TestOnAndEmitWithThreeArgsCheckSumMocksObjInListenerFunction', () => {
+        const eventBus = new EventBus();
+        const x = 1;
+        const y = 1;
+        const mockSum = { sum: 0 };
+    
+        const expectedSumXY = x + y;
+    
+        eventBus.on('event1', function(_x, _y, sum) { 
+            sum.sum = _x + _y;
+        });
+        eventBus.emit('event1', x, y, mockSum);
+    
+        const actualSumXY = mockSum.sum;
+
+        CHECK_ACTUAL_EQUAL_EXPECTED(actualSumXY, expectedSumXY);
+    }),
+     
+    
      
 );
